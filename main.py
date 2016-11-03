a8e421e2c6d4e7e34aa697a956f80997572197d1
Michal <mikh3x4@gmail.com>
2016-11-03 17:18:26 +0000

    added back motors

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ a = Subscriber("f t", b"ff", 8830)
 print("finding an odrives...")
 middle_odrive = odrive.find_any(serial_number="208037853548")
 front_odrive = odrive.find_any(serial_number="2062376E3548")
+back_odrive = odrive.find_any(serial_number="208037843548")
 print("found")
 
-middle_odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-middle_odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-front_odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-front_odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+middle_odrive.axis0.requested_state = AXIS_STATE_IDLE
+middle_odrive.axis1.requested_state = AXIS_STATE_IDLE
+front_odrive.axis0.requested_state = AXIS_STATE_IDLE
+front_odrive.axis1.requested_state = AXIS_STATE_IDLE
+back_odrive.axis0.requested_state = AXIS_STATE_IDLE
+back_odrive.axis1.requested_state = AXIS_STATE_IDLE
 
 while True:
     try:
@@ -25,25 +28,37 @@ while True:
             middle_odrive.axis1.requested_state = AXIS_STATE_IDLE
             front_odrive.axis0.requested_state = AXIS_STATE_IDLE
             front_odrive.axis1.requested_state = AXIS_STATE_IDLE
+            back_odrive.axis0.requested_state = AXIS_STATE_IDLE
+            back_odrive.axis1.requested_state = AXIS_STATE_IDLE
         else:
             middle_odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             middle_odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             front_odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             front_odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+            back_odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+            back_odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
             middle_odrive.axis0.controller.vel_setpoint = -(msg.f + msg.t)
             middle_odrive.axis1.controller.vel_setpoint = (msg.f - msg.t)
             front_odrive.axis0.controller.vel_setpoint = (msg.f - msg.t)
             front_odrive.axis1.controller.vel_setpoint = -(msg.f + msg.t)
+
+            # back odrive is reversed left to right
+            back_odrive.axis1.controller.vel_setpoint = (msg.f - msg.t)
+            back_odrive.axis0.controller.vel_setpoint = -(msg.f + msg.t)
     except:
         pass
         middle_odrive.axis0.requested_state = AXIS_STATE_IDLE
         middle_odrive.axis1.requested_state = AXIS_STATE_IDLE
         front_odrive.axis0.requested_state = AXIS_STATE_IDLE
         front_odrive.axis1.requested_state = AXIS_STATE_IDLE
+        back_odrive.axis0.requested_state = AXIS_STATE_IDLE
+        back_odrive.axis1.requested_state = AXIS_STATE_IDLE
         middle_odrive.axis0.controller.vel_setpoint = 0
         middle_odrive.axis1.controller.vel_setpoint = 0
         front_odrive.axis0.controller.vel_setpoint = 0
         front_odrive.axis1.controller.vel_setpoint = 0
+        back_odrive.axis0.controller.vel_setpoint = 0
+        back_odrive.axis1.controller.vel_setpoint = 0
         raise
 
