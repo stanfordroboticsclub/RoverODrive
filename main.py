f8b670f4f5f3beb336f9d168e321fc611b3da0c0
Michal <mikh3x4@gmail.com>
2018-09-28 00:07:24 +0000

    added both motors

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 import odrive
+from odrive.enums import *
+
 from UDPComms import Subscriber
 
 a = Subscriber("f t", b"ff", 8830)
 
+print("finding an odrive...")
+odrv0 = odrive.find_any()
+print("found")
+
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
 while True:
     try:
-            msg = a.recv()
-            odrv0.axis0.controller.vel_setpoint = msg.f + msg.f
-            odrv0.axis1.controller.vel_setpoint = -(msg.f - msg.f)
+        msg = a.recv()
+        print(msg)
+        odrv0.axis0.controller.vel_setpoint = -(msg.f + msg.t)
+        odrv0.axis1.controller.vel_setpoint = (msg.f - msg.t)
     except:
         pass
         odrv0.axis0.controller.vel_setpoint = 0
