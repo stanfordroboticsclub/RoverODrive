adf41584d713f676fcdc8d1eb6301b18e061b8d9
MichalAdam <mikh3x4@gmail.com>
2018-11-11 12:14:09 -0800

    made odrive setup automated

--- odrive_setup.py
+++ odrive_setup.py
@@ -1,6 +1,32 @@
+#!/usr/bin/env python3
 
+import odrive
+from odrive.enums import *
+import time
+
+reply = input("""This script will calibrate an odrive for use.
+It will spin the motors as part of the process so make sure they are 
+free to spin. If multiple odrives are connected it will calibrate a
+random one so keep that in mind.
+
+Do you want to proceed? [Y/n]""")
+
+
+if reply != "Y":
+    print("Aborting...")
+    exit()
+else:
+    print("Starting setup")
+
+odrv0 = odrive.find_any()
+print("Found ODrive", odrv0.serial_number)
+time.sleep(3)
+
+# this is important so the odrive knows it can't dump
+# energy into the non existent brake resistor
 odrv0.config.brake_resistance = 0
 
+# configure axis 0
 
 odrv0.axis0.motor.config.pole_pairs = 15
 odrv0.axis0.motor.config.resistance_calib_max_voltage = 4
@@ -17,7 +43,7 @@ odrv0.axis0.controller.config.vel_integrator_gain = 0.1
 odrv0.axis0.controller.config.vel_limit = 1000
 odrv0.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
 
-
+# configure axis 1
 
 odrv0.axis1.motor.config.pole_pairs = 15
 odrv0.axis1.motor.config.resistance_calib_max_voltage = 4
@@ -36,23 +62,24 @@ odrv0.axis1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
 
 
 odrv0.save_configuration()
-odrv0.reboot()
 
 
+# axis 0
 odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
-odrv0.axis0.motor
+assert odrv0.axis0.motor.error == 0
 odrv0.axis0.motor.config.pre_calibrated = True
 
 odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
-odrv0.axis0.encoder
+assert odrv0.axis0.encoder.error == 0
 odrv0.axis0.encoder.config.pre_calibrated = True
 
+# axis 1
 odrv0.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
-odrv0.axis1.motor
+assert odrv0.axis1.motor.error == 0
 odrv0.axis1.motor.config.pre_calibrated = True
 
 odrv0.axis1.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
-odrv0.axis1.encoder
+assert odrv0.axis1.encoder.error == 0
 odrv0.axis1.encoder.config.pre_calibrated = True
 
 # still looking for good values
@@ -62,11 +89,11 @@ odrv0.axis1.motor.config.current_lim = 25
 odrv0.save_configuration()
 odrv0.reboot()
 
+print("Done!")
+exit()
 
-
-odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-odrv0.axis0.controller.vel_setpoint = 120
-
+# odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# odrv0.axis0.controller.vel_setpoint = 120
 
 def drive(odrive, speed):
      odrive.axis1.controller.vel_setpoint = speed
@@ -80,16 +107,3 @@ def on(odrive):
      odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
      odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-
-
-pi@raspberrypi:~$ sudo odrivetool --path serial:/dev/ttyS0 --verbose
-ODrive control utility v0.4.2
-Waiting for ODrive...
-Connecting to device on serial port /dev/ttyS0@115200
-Please connect your ODrive.
-You can also type help() or quit().
-
-In [1]:
-
-no response - probably incompatible
-In [1]:
