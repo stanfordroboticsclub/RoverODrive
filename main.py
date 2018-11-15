c446f99604bb5a33e978094c3d281fd59ab5b640
Michal <mikh3x4@gmail.com>
2018-11-15 04:12:41 +0000

    updates odrive serial numbers

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ telemetry = Publisher("bat_voltage F0_cur F1_cur M0_cur M1_cur B0_cur B1_cur", b
 
 print("finding an odrives...")
 middle_odrive = odrive.find_any(serial_number="208037853548")
-front_odrive = odrive.find_any(serial_number="2062376E3548")
+print("found middle odrive")
+front_odrive = odrive.find_any(serial_number="376136493137")
+print("found front odrive")
 back_odrive = odrive.find_any(serial_number="208037843548")
-print("found")
+print("found back odrive")
+print("found all odrives")
 
 middle_odrive.axis0.requested_state = AXIS_STATE_IDLE
 middle_odrive.axis1.requested_state = AXIS_STATE_IDLE
