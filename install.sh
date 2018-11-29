300dfbf230949ea30dbfa66472ead518a0cd96f0
Michal <mikh3x4@gmail.com>
2018-11-29 11:29:54 +0000

    install now auto detects service files

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+FOLDER=$(dirname $(realpath "$0"))
+cd $FOLDER
+
 yes | sudo pip3 install odrive
 
-FOLDER=$(dirname $(realpath "$0"))
+for file in *.service; do
+    [ -f "$file" ] || break
+    sudo ln -s $FOLDER/$file /lib/systemd/system/
+done
 
-sudo ln -s $FOLDER/odrive.service /lib/systemd/system/
