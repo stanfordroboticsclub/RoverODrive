8f3ee8114cdc7d7c127830e15529691269dbe68d
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-01-13 14:27:53 -0800

    moved to new udpcomms

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os
 if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
-cmd = Subscriber("f t", b"ff", 8830, timeout = 0.3)
-telemetry = Publisher("bat_voltage F0_cur F1_cur M0_cur M1_cur B0_cur B1_cur", b"f6f", 8810)
+cmd = Subscriber(8830, timeout = 0.3)
+# telemetry = Publisher(8810)
 
 print("finding an odrives...")
 middle_odrive = odrive.find_any(serial_number="208037853548")
@@ -33,15 +33,15 @@ while True:
         msg = cmd.get()
         print(msg)
 
-        telemetry.send( middle_odrive.vbus_voltage,
-                        front_odrive.axis0.motor.current_control.Iq_measured,
-                        front_odrive.axis1.motor.current_control.Iq_measured,
-                        middle_odrive.axis0.motor.current_control.Iq_measured,
-                        middle_odrive.axis1.motor.current_control.Iq_measured,
-                        back_odrive.axis0.motor.current_control.Iq_measured,
-                        back_odrive.axis1.motor.current_control.Iq_measured)
+        # telemetry.send( middle_odrive.vbus_voltage,
+        #                 front_odrive.axis0.motor.current_control.Iq_measured,
+        #                 front_odrive.axis1.motor.current_control.Iq_measured,
+        #                 middle_odrive.axis0.motor.current_control.Iq_measured,
+        #                 middle_odrive.axis1.motor.current_control.Iq_measured,
+        #                 back_odrive.axis0.motor.current_control.Iq_measured,
+        #                 back_odrive.axis1.motor.current_control.Iq_measured)
 
-        if (msg.t == 0 and msg.f == 0):
+        if (msg['t'] == 0 and msg['f'] == 0):
             middle_odrive.axis0.requested_state = AXIS_STATE_IDLE
             middle_odrive.axis1.requested_state = AXIS_STATE_IDLE
             front_odrive.axis0.requested_state = AXIS_STATE_IDLE
@@ -56,14 +56,14 @@ while True:
             back_odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             back_odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-            middle_odrive.axis0.controller.vel_setpoint = -(msg.f + msg.t)
-            middle_odrive.axis1.controller.vel_setpoint = (msg.f - msg.t)
-            front_odrive.axis0.controller.vel_setpoint = (-msg.f - msg.t)
-            front_odrive.axis1.controller.vel_setpoint = -(-msg.f + msg.t)
+            middle_odrive.axis0.controller.vel_setpoint = -(msg['f'] + msg['t'])
+            middle_odrive.axis1.controller.vel_setpoint = (msg['f'] - msg['t'])
+            front_odrive.axis0.controller.vel_setpoint = (-msg['f'] - msg['t'])
+            front_odrive.axis1.controller.vel_setpoint = -(-msg['f'] + msg['t'])
 
             # back odrive is reversed left to right
-            back_odrive.axis1.controller.vel_setpoint = (msg.f - msg.t)
-            back_odrive.axis0.controller.vel_setpoint = -(msg.f + msg.t)
+            back_odrive.axis1.controller.vel_setpoint = (msg['f'] - msg['t'])
+            back_odrive.axis0.controller.vel_setpoint = -(msg['f'] + msg['t'])
     except timeout:
         middle_odrive.axis0.requested_state = AXIS_STATE_IDLE
         middle_odrive.axis1.requested_state = AXIS_STATE_IDLE
--- single.py
+++ single.py
@@ -9,7 +9,7 @@ import os
 if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
-a = Subscriber("f t", b"ff", 8830, timeout = 0.3)
+a = Subscriber(8830, timeout = 0.3)
 
 print("finding any odrives...")
 odrive = odrive.find_any()
@@ -22,15 +22,15 @@ while True:
     try:
         msg = a.get()
         print(msg)
-        if (msg.t == 0 and msg.f == 0):
+        if (msg['t'] == 0 and msg['f'] == 0):
             odrive.axis0.requested_state = AXIS_STATE_IDLE
             odrive.axis1.requested_state = AXIS_STATE_IDLE
         else:
             odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
             odrive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-            odrive.axis0.controller.vel_setpoint = (msg.f + msg.t)
-            odrive.axis1.controller.vel_setpoint = -(msg.f - msg.t)
+            odrive.axis0.controller.vel_setpoint = (msg['f'] + msg['t'])
+            odrive.axis1.controller.vel_setpoint = -(msg['f'] - msg['t'])
     except timeout:
         odrive.axis0.requested_state = AXIS_STATE_IDLE
         odrive.axis1.requested_state = AXIS_STATE_IDLE
