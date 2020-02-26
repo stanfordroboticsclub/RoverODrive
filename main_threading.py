dad0c149a257205c6a5bda566ee0aa744785bb16
Michal <mikadam@stanford.edu>
2020-02-26 04:59:09 +0000

    moved to new serial numbers

--- main_threading.py
+++ main_threading.py
@@ -20,9 +20,9 @@ telemetry = Publisher(8810)
 
 # motor 0 is right
 # motor 1 is left
-odrives = [ ['middle' , "207B37883548", [1, 1]],
-            ['front', "207D37A33548",  [-1, 1]],
-            ['back'  , "207B37813548", [-1, 1]]]
+odrives = [ ['middle', "208D358D524B", [-1, 1]],
+            ['front' , "2092358D524B", [-1, 1]],
+            ['back'  , "2092357A524B", [-1, 1]]]
 
 def clear_errors(odv):
     if odv.axis0.error:
@@ -98,9 +98,9 @@ def run_odrive(name, serial_number, d):
                     # axis 0 (right) is always same sign
                     # axis 1 (left) is always opposite sign
                     odv.axis0.controller.vel_setpoint =  d[0]*( \
-                                msg['f'] + msg['t'])
-                    odv.axis1.controller.vel_setpoint =  d[1]*( \
                                 msg['f'] - msg['t'])
+                    odv.axis1.controller.vel_setpoint =  d[1]*( \
+                                msg['f'] + msg['t'])
                     odv.axis0.watchdog_feed()
                     odv.axis1.watchdog_feed()
 
